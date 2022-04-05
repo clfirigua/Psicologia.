c911097b9262573627b4d46f0f89599e2f1c701d
clfirigua <71801587+clfirigua@users.noreply.github.com>
2022-04-04 21:31:12 -0500

    pruebas

--- pages/forms/forms.js
+++ pages/forms/forms.js
@@ -1,4 +1,11 @@
 import {menu} from "../../shared/menu.js"
 
+const btn = document.getElementById('newForm');
 
 menu()
+
+
+btn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    location.href ='../newForm/newForm.html';
+})
