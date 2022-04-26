6dae1b12766e5b314f8db0781498b81b3ae681df
Carlos Andres Firigua Contreras <71801587+clfirigua@users.noreply.github.com>
2022-04-25 19:04:35 -0500

    new Forms templete

--- pages/newForm/newForm.js
+++ pages/newForm/newForm.js
@@ -1,173 +1,137 @@
-import { question } from "../../components/questions.js";
-import { updateData, arrayUnion, arrayRemove, doc, db, onSnapshot } from "../../services/crudservice.js";
-import { updateDoc } from "../../services/firebaseservice.js";
+import { updateData, doc, db, onSnapshot, getData, onGetDocument} from "../../services/crudservice.js";
 import { menu } from "../../shared/menu.js";
 
-const nameForm = document.getElementById("nombreFormulario")
-const localname = localStorage.getItem("Nameform")
-const addQuestion = document.getElementById("añadirPreguntas")
-const modalVaremo = document.getElementById("varemos");
-const inpVaremo = document.getElementById("recipient-name");
-const btnvaremo = document.getElementById("btnGuardarVaremo");
-const btnRespuesta = document.getElementById("btnGuardarRespuesta");
-const btnGuardar = document.getElementById("guardarPregunta");
-const inpRespuesta = document.getElementById("inpRespuestas");
-const idForm = localStorage.getItem("idForm");
-menu();
-mostrarFormulario();
-cargarFormularios();
+const idFormulario = localStorage.getItem('idForm');
 
-function mostrarFormulario() {
-  question.forEach(data => {
-    switch (data.type) {
-      case "text":
-        $(addQuestion).append(
-          `
-          <label for="${data.id}" class="form-label">${data.texto}</label>
-          <input type="${data.type}" class="form-control mb-2" placeholder="${data.placeholder}" id="${data.id}">
-          `
-        );
-        break;
-      case "select":
-        $(addQuestion).append(
-          `
-        <label for="${data.id}" class="form-label">${data.texto}</label>
-        <select class="form-control mb-2" id="${data.id}">
-          <option selected disabled value="false">${data.placeholder}</option>
-        </select>
-        `
-        );
-        break;
-      case "button":
-        $(addQuestion).append(
-          `
-          <input type="${data.type}" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#modalRespuestas" value="Agregar Respuestas">
-          `
-        );
-        break;
-      case "tpselect":
-        $(addQuestion).append(
-          `
-            <label for="${data.id}" class="form-label">${data.texto}</label>
-            <select class="form-control mb-2" id="${data.id}">
-              <option value="null" selected disabled>${data.placeholder}</option>
-              <option value="seleccion unica" >Seleccion unica</option>
-              <option value="seleccion multiple" >Seleccion multiple</option>
-            </select>
-            `
-        );
-        break;
-      default:
-        alert("Contacta con el administrador del sistema o equipo de desarrollo.")
-        break;
-    }
+const inptPregunta = document.getElementById('pregunta');
+const inptNombreVaremo = document.getElementById('recipient-name');
+const inptNombreRespuesta = document.getElementById('inpRespuestas');
+
+const selectTipoRespuesta = document.getElementById('tpRespuesta');
+const selectPreguntaDepende = document.getElementById('dPregunta');
+const selectRespuestaDepende = document.getElementById('dRespuesta');
+const selectVaremo = document.getElementById('varemo');
+
+const btnGuardarPregunta = document.getElementById('guardarPregunta');
+const btnGuardarVaremo = document.getElementById('btnGuardarVaremo');
+const btnGuardarRespuesta = document.getElementById('btnGuardarRespuesta');
+
+const divVaremos = document.getElementById('varemos');
+
+
+let Preguntas = [];
+let varemos = [];
+let respuestas = [];
+
+
+const dataForm = () =>{
+  return{
+    nombrePregunta: inptPregunta.value,
+    tipoDeRespuesta : selectTipoRespuesta.value,
+    preguntaDepende: selectPreguntaDepende.value,
+    respuestaDepende: selectRespuestaDepende.value,
+    varemo: selectVaremo.value,
+    respuestas:[]
+  }
+}
+
+const generarVaremos = (varemos = []) =>{
+  divVaremos.innerHTML = '';
+  selectVaremo.innerHTML = '<option value="false">Varemo de medicion</option>';
+
+  if(varemos.length === 0){
+    $(divVaremos).append(`
+      <p>Sin varmos Cargados</p>
+    `);
+    return
+  }
+ 
+  varemos.forEach(varemo => {
+    $(selectVaremo).append(`
+      <option value="${varemo}">${varemo}</option>
+    `);
+  });
+
+}
+
+const generarTargetas = () =>{
+  
+}
+
+const generarPReguntas = (preguntas = []) =>{
+  if(preguntas.length == 0){
+    return
+  }
+
+  preguntas.forEach((data,index)=>{
+    $(selectPreguntaDepende).append(`
+      <option value="${index}">${data.nombrePregunta}</option>
+    `);
   })
 }
 
-const preguntaDepende = document.getElementById("dPregunta");
-const respuestaDepende = document.getElementById("dRespuesta");
-const selectVaremo = document.getElementById("varemo");
-const agregarRespuestas = document.getElementById("agregarRespuestas");
+const cargarDatosForm = () =>{
+  onSnapshot(doc(db, "formularios", idFormulario), (doc) => {
 
+    Preguntas = doc.data().preguntas;
+    varemos = doc.data().varemoMedicion;
 
-function cargarFormularios() {
-  onSnapshot(doc(db, "formularios", idForm), (doc) => {
-    //declaramos variables usadas en la base de datos
-    let varemos = doc.data().varemoMedicion
-    let preguntas = doc.data().preguntas
-    let nombre = doc.data().nombre
-    let datosForm = doc.data()
+    generarPReguntas(Preguntas)
+    generarTargetas(Preguntas);
+    generarVaremos(varemos);
 
-
-
-    nameForm.innerHTML = nombre
-
-    modalVaremo.innerHTML = ""
-
-
-    selectVaremo.innerHTML= `<option selected disabled value="false">Varemo de medicion</option>`
-    //si depende de alguna pregunt
-    // const preguntas2 = [];
-    btnRespuesta.addEventListener("click", async (e)=>{
-      e.preventDefault()
-      
-      if(doc.data().preguntas.lenght == undefined){
-        datosForm.preguntas.push({
-          respuesta:inpRespuesta.value
-        })
-        // datosForm.preguntas = preguntas2;
-        updateData(doc.id,datosForm,'formularios')
-        console.log('exito')
-        
-      }
-      
-    })
-
-    if(preguntas.lenght > 0){
-      preguntas.forEach((data,i) => {
-      
-        $(preguntaDepende).append(
-          `
-            <option value="${i}" class="seleccionar" >${data.pregunta}</option>
-            `
-        )
-      })
-    }
-
-    //varemos form
-    try{
-    varemos.forEach((data,i) => {
-      $(selectVaremo).append(
-        `
-          <option value="${i}" class="seleccionar" >${data}</option>
-          `
-      )
-      $(modalVaremo).append(
-        `
-    <div class="row">
-      <p class="col-sm-10">${data}</p>
-      <i class="fas fa-trash red col-sm-2 eliminar" data-id="${data}" ></i>
-    </div>
-
-    `
-      )
-      const eliminar = document.querySelectorAll(".eliminar");
-      eliminar.forEach(btn => {
-        btn.addEventListener("click", (e) => {
-
-          updateData(idForm, { varemoMedicion: arrayRemove(e.target.dataset.id) }, "formularios")
-        })
-      })
-    })}
-    catch{
-      console.log("error");
-    }
-
-        // respuesta dependiente
-    $(preguntaDepende).on("change", function (e) {
-      
-      const idFormPregunta = $(this).val();
-      let respuestasForm = preguntas[idFormPregunta].Respuestas;
-
-      respuestaDepende.innerHTML=""
-      respuestasForm.forEach((data,i) => {
-
-        $(respuestaDepende).append(
-          `
-            <option value="${i}" class="seleccionar" >${data}</option>
-            `
-        )
-      })
-    })
   })
 }
-btnvaremo.addEventListener("click", (e) => {
-  updateData(idForm, { varemoMedicion: arrayUnion(inpVaremo.value) }, "formularios")
-  inpVaremo.value = ""
+
+btnGuardarPregunta.addEventListener('click', (event)=>{
+  const formTerminado = dataForm();
+  if(respuestas.length == 0 ){
+    alert('No tienes respuestas Cargadas');
+    return
+  }
+
+  formTerminado.respuestas = respuestas;
+  Preguntas.push(formTerminado);
+
+  updateData(idFormulario,{preguntas:Preguntas},'formularios');
 })
 
-btnGuardar.addEventListener('click', (e)=>{
-  e.preventDefault();
-  console.log('hola');
+btnGuardarVaremo.addEventListener('click', (event)=>{
+
+  varemos.push(inptNombreVaremo.value);
+  inptNombreVaremo.value = "";
+  updateData(idFormulario, {varemoMedicion:varemos},'formularios');
+  
 })
 
+btnGuardarRespuesta.addEventListener('click', (event)=>{
+  
+  respuestas.push(inptNombreRespuesta.value);
+  inptNombreRespuesta.value  = '';
+  
+})
+
+
+$(selectPreguntaDepende).on("change", function (e) {
+      
+  const idFormPregunta = $(this).val();
+  let respuestasForm = Preguntas[idFormPregunta].respuestas;
+
+  selectRespuestaDepende.innerHTML="";
+  respuestasForm.forEach((data,i) => {
+    $(selectRespuestaDepende).append(
+      `
+        <option value="${i}" class="seleccionar" >${data}</option>
+        `
+    )
+  }
+  )
+})
+
+$(document).ready(function () {
+
+  menu();
+  cargarDatosForm();
+
+
+});
