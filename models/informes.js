a6c7280fbbda28226bca20810e8ec956e1f21682
aceros123 <57160341+aceros123@users.noreply.github.com>
2022-09-21 22:34:23 -0500

    modelo de informes y respuestas, posibles correcciones a futuro

---
+++ models/informes.js
@@ -0,0 +1,9 @@
+const informes = {
+    formulario:String, // opcional
+    usuario:String, // opcional
+    respuestas:String,
+    informe:[{
+        indexbaremo:number,
+        calculo:number
+    }]
+}
