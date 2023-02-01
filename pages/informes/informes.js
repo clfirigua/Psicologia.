75158c506741c9d4a149e43710cb85723ec96cf6
aceros123 <57160341+aceros123@users.noreply.github.com>
2023-01-31 23:04:25 -0500

    cambio de grafico a lineas

--- pages/informes/informes.js
+++ pages/informes/informes.js
@@ -77,12 +77,12 @@ function cargarResultados(idFormulario, idUsuario) {
     baremos.forEach((baremo,index) => {
 
       cargarRespuestas(idUsuario, baremo.baremo).then((result) => {
-        // console.log(result.baremo, result.suma);
+
         z = (result.suma - baremo.media) / baremo.desviacion;
         t = (z * 10) + 50
         listBaremo.push({
           baremo: baremo.baremo,
-          z: (z*z)
+          t: t
         });
         $(tablaInformes).append(`
       <tr>
@@ -93,7 +93,6 @@ function cargarResultados(idFormulario, idUsuario) {
         <td>${z.toFixed(2)}</td>
         <td>${t.toFixed(2)}</td>
       </tr>`);
-      console.log(baremos);
       
       if (index == baremos.length-1) {
         grafica(listBaremo)
@@ -137,94 +136,85 @@ exportar.addEventListener('clic', ()=>{
 
  function  grafica  (list = [""]) {
 
-  console.log(list)
-  am5.ready(function () {
+    am5.ready(function() {
 
-    // Create root element
-    // https://www.amcharts.com/docs/v5/getting-started/#Root_element
-    var root = am5.Root.new("chartdiv");
+      // Create root element
+      // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+      var root = am5.Root.new("chartdiv");
+            
+      // Set themes
+      // https://www.amcharts.com/docs/v5/concepts/themes/
+      root.setThemes([
+        am5themes_Animated.new(root)
+      ]);    
 
+      // Create chart
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/
+      var chart = root.container.children.push(am5xy.XYChart.new(root, {
+        panX: true,
+        panY: true,
+        wheelX: "panX",
+        wheelY: "zoomX",
+        pinchZoomX:true
+      }));
+      
+      // Add cursor
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+      var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
+      cursor.lineY.set("visible", false);
+        
+      // Create axes
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+          var xAxis = chart.xAxes.push(
+            am5xy.CategoryAxis.new(root, {
+              categoryField: "baremo",
+              renderer: am5xy.AxisRendererX.new(root, {}),
+            })
+          );
 
-    // Set themes
-    // https://www.amcharts.com/docs/v5/concepts/themes/
-    root.setThemes([
-      am5themes_Animated.new(root)
-    ]);
+      var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+        maxDeviation: 0.3,
+        renderer: am5xy.AxisRendererY.new(root, {})
+      }));
+           
+      // Add series
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+      var series = chart.series.push(am5xy.LineSeries.new(root, {
+        name: "baremo",
+        xAxis: xAxis,
+        yAxis: yAxis,
+        valueYField: "t",
+        categoryXField: "baremo",
+        tooltip: am5.Tooltip.new(root, {
+          labelText: "{categoryX}: {valueY}"
+        })
+      }));
+        
+      // Add scrollbar
+      // https://www.amcharts.com/docs/v5/charts/xy-chart/scrollbars/
+      chart.set("scrollbarX", am5.Scrollbar.new(root, {
+        orientation: "horizontal"
+      }));
+      
+      // Set data
+      var data = list
+      xAxis.data.setAll(data);
+      series.data.setAll(data);
 
+      //create bullets
+      series.bullets.push(function () {
+        return am5.Bullet.new(root, {
+          locationX:undefined,
+          sprite: am5.Circle.new(root, {
+            radius: 4,
+            fill: series.get("fill")
+          })
+        })
+      });
+      // Make stuff animate on load
+      // https://www.amcharts.com/docs/v5/concepts/animations/
+      series.appear(1000);
+      chart.appear(1000, 100);
+      });
 
-    // Create chart
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/
-    var chart = root.container.children.push(am5xy.XYChart.new(root, {
-      panX: true,
-      panY: true,
-      wheelX: "panX",
-      wheelY: "zoomX",
-      pinchZoomX: true
-    }));
-
-    // Add cursor
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-    var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-    cursor.lineY.set("visible", false);
-
-
-    // Create axes
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-    var xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
-    xRenderer.labels.template.setAll({
-      rotation: -90,
-      centerY: am5.p50,
-      centerX: am5.p100,
-      paddingRight: 15
-    });
-
-    var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-      maxDeviation: 0.3,
-      categoryField: "baremo",
-      renderer: xRenderer,
-      tooltip: am5.Tooltip.new(root, {})
-    }));
-
-    var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-      maxDeviation: 0.3,
-      renderer: am5xy.AxisRendererY.new(root, {})
-    }));
-
-    console.log(list)
-    // Create series
-    // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-    var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-      name: "Series 1",
-      xAxis: xAxis,
-      yAxis: yAxis,
-      valueYField: "z",
-      sequencedInterpolation: true,
-      categoryXField: "baremo",
-      tooltip: am5.Tooltip.new(root, {
-        labelText: "{valueY}"
-      })
-    }));
-
-    series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5 });
-    series.columns.template.adapters.add("fill", function (fill, target) {
-      return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    series.columns.template.adapters.add("stroke", function (stroke, target) {
-      return chart.get("colors").getIndex(series.columns.indexOf(target));
-    });
-
-    var data = list
-    // Set data
-
-    xAxis.data.setAll(data);
-    series.data.setAll(data);
-
-
-    // Make stuff animate on load
-    // https://www.amcharts.com/docs/v5/concepts/animations/
-    series.appear(1000);
-    chart.appear(1000, 100);
-
-  }); // end am5.ready()
-} // end am5.ready()
+}
--- shared/menu.js
+++ shared/menu.js
@@ -3,37 +3,24 @@ import { getData } from "../services/crudservice.js";
 const listaDatos = document.getElementById('listaDatos')
 
 
-
+console.log(roles[0].texto);
 
 const menu = async(arr) =>{
 
     const userLocal = JSON.parse(localStorage.getItem('user'));
     const valores = await getData(userLocal.rol, 'roles');
-    let a = []
-    Object.entries(valores.data()).forEach((data)=>{
+    console.log(valores.data());
+    for(let i=0; i<roles.length; i++){
+        console.log(i);
+    }
 
+    Object.entries(valores.data()).forEach((data)=>{
+        console.log(data);
         for (let i = 0; i < roles.length; i++) {
-            if(data[0] ==  roles[i].slug && data[1] == true ){
-                a.push(roles[i]);
-                
-            //     $(listaDatos).append(`
-            //     <li class="nav-item m-3 text-capitalize">
-            //         <a class="nav-link " aria-current="page" href="${roles[i].ruta}">${roles[i].texto}</a>
-            //     </li>
-            // `)
-            }
-        }
-    })
-    a = roles.sort((a, b) => a.slug.localeCompare(b.slug))
-
-    Object.entries(valores.data()).forEach((data)=>{
-        
-        for (let i = 0; i < a.length; i++) {
-            if(a[i].slug==data[0] && data[1] == true ){                
-                
+            if( roles[i].slug == data[0]  && data[1] == true ){
                 $(listaDatos).append(`
                 <li class="nav-item m-3 text-capitalize">
-                    <a class="nav-link " aria-current="page" href="${a[i].ruta}">${a[i].texto}</a>
+                    <a class="nav-link " aria-current="page" href="${roles[i].ruta}">${roles[i].texto}</a>
                 </li>
             `)
             }
